9bbcc898b3abc8eeaa98ad610c9610bc3e4a7c0d
Floriannee <floriannepan@gmail.com>
2026-05-12 18:56:26 +0900

    Refonte de la page equipe, uniformisation du style des photos et correction des chemins d images

--- src/app/pages/equipe/equipe.ts
+++ src/app/pages/equipe/equipe.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-equipe',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './equipe.html',
   styleUrl: './equipe.css'
 })
 export class Equipe {
-
+  members = [
+    {
+      name: 'Dr Amélie Belinchon Sanchez',
+      role: 'Chirurgien dentiste',
+      img: 'assets/img/dr_belinchon.PNG'
+    },
+    {
+      name: 'Dr Aurore Daoudi',
+      role: 'Chirurgien dentiste',
+      img: 'assets/img/dr_daoudi.PNG'
+    },
+    {
+      name: 'Dr Jenny Ye',
+      role: 'Chirurgien dentiste',
+      img: 'assets/img/dr_ye.PNG'
+    },
+    {
+      name: 'Dr Marine Evrard',
+      role: 'Chirurgien dentiste',
+      img: 'assets/img/dr_evrard.PNG'
+    },
+    {
+      name: 'Dr Clémence Kuntz',
+      role: 'Chirurgien dentiste',
+      img: 'assets/img/dr_kuntz.PNG'
+    },
+    {
+      name: 'Dr Matthieu Juillet',
+      role: 'Chirurgien dentiste',
+      img: 'assets/img/dr_juillet.PNG'
+    }
+  ];
 }
+
